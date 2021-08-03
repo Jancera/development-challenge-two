5855604f932967e7b818cdc838a20876de1d1dfd
Jancera <jancerboy@gmail.com>
2021-08-03 01:10:29 -0300

    Connecting to the backend

---
+++ src/api/aws.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL:
+    "https://9lnxingcgi.execute-api.sa-east-1.amazonaws.com/dev",
+});
--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import {
   TextField,
   Container,
   Typography,
   Button,
 } from "@material-ui/core";
-import axios from "axios";
+import aws from "../../api/aws";
 import useStyles from "./searchStyles";
 import searchDrawing from "../../assets/search.svg";
 
-const data = axios.patch();
-
 const Search = () => {
+  const [id, setId] = useState("");
   const classes = useStyles();
+
+  const getUser = (user) => {
+    try {
+      const data = aws.get(`/${user}`);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className={classes.container}>
       <div className={classes.drawingContainer}>
@@ -38,6 +48,8 @@ const Search = () => {
             required
             fullWidth
             className={classes.input}
+            value={id}
+            onChange={(event) => setId(event.target.value)}
           />
           <Button
             className={classes.button}
@@ -45,7 +57,7 @@ const Search = () => {
             color="secondary"
             size="large"
             type="submit"
-            onClick={() => console.log("clicked")}
+            onClick={getUser(id)}
           >
             Pesquisar
           </Button>
